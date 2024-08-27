e45c9360c6d1875248a78fdd6a627ea9659a38a4
usuario <usuario@gmail.com>
2024-08-27 12:08:03 -0300

    Resolvendo 1781

---
+++ strings/Guga e String/1781.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Letter{
+    char value;
+    int pos;
+};
+
+bool eVogal(char ch){
+    return 'a' == ch || 'e' == ch || 'i' == ch || 'o' == ch || 'u' == ch;
+}
+
+vector<Letter> separateLetter(string &text, bool vogals){
+    vector<Letter> list;
+    int ts = text.size();
+    for (int i = 0; i < ts; ++i) {
+        char ch = text[i];
+        bool isVowel = eVogal(ch);
+
+        if (vogals && isVowel) {
+            list.push_back(Letter{ch, i});
+        } else if (!vogals && !isVowel) {
+            list.push_back(Letter{ch, i});
+        }
+    }
+    return list;
+}
+
+void tranform_string(string &text, int deslocamento, bool vogais){
+    vector<Letter> lletter = separateLetter(text, true);
+    int i = 0;
+
+    for(Letter letter : lletter){
+        int segunda_pos = i;
+        if (lletter.size()&1){
+            cout << "PAAAAAAAAAAA";
+            segunda_pos = (i + 2) % lletter.size();
+        }
+        int target_pos = (segunda_pos + deslocamento) % lletter.size();
+        text[letter.pos] = lletter[target_pos].value;
+        i++;
+    }
+}
+
+
+int main(){
+    int numTests;
+    cin >> numTests;
+
+    while (numTests--) {
+        string text;
+        cin >> text;
+
+        int numInstructions;
+        cin >> numInstructions;
+
+        while (numInstructions--) {
+            int tipo, passos;
+            cin >> tipo;
+            switch (tipo){
+            case 0:
+                cin >> passos;
+                tranform_string(text, passos, true);
+                break;
+            case 1:
+                cin >> passos;
+                tranform_string(text, passos, false);
+                break;
+            default:
+                cout << text << endl;
+                break;
+            }
+        }
+    }
+    return 0;
+}
---
+++ strings/Guga e String/my.sol
@@ -0,0 +1,3 @@
+campotodur
+abecidfugh
+ebicudfagh
